74fd9ccccfe3289c4bea98f030e7494e5251144e
Ichlasul Fikri <ichlasul.ap@gmail.com>
2024-04-27 17:17:37 +0700

    Scrollable User Input

--- app/src/main/java/id/rashio/android/MainActivity.kt
+++ app/src/main/java/id/rashio/android/MainActivity.kt
@@ -1,13 +1,13 @@
 package id.rashio.android
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,7 +19,7 @@ import id.rashio.android.ui.theme.AppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContent {
             AppTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/id/rashio/android/ui/screen/register/RegisterScreen.kt
+++ app/src/main/java/id/rashio/android/ui/screen/register/RegisterScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -53,8 +55,9 @@ import id.rashio.android.ui.theme.poppinsFontFamily
 fun RegisterScreen(modifier: Modifier = Modifier, navController: NavController) {
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
-            .padding(horizontal = 28.dp),
+            .padding(horizontal = 28.dp, vertical = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
