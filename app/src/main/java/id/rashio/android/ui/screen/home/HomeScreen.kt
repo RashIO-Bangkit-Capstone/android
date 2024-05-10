83e020d54dc9495adef652af67d3f4b799a90ac8
Ichlasul Fikri <ichlasul.ap@gmail.com>
2024-05-10 15:50:15 +0700

    chore: refactor HomeScreen and HomeScreenViewModel

--- app/src/main/java/id/rashio/android/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/id/rashio/android/ui/screen/home/HomeScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
