5d19eec3379e93c49c404f0d08f65d4091a18144
Ichlasul Fikri <ichlasul.ap@gmail.com>
2024-06-11 12:17:55 +0700

    chore: add vertical scroll for ArticleDetailScreen

--- app/src/main/java/id/rashio/android/ui/screen/articles/article_detail/ArticleDetailScreen.kt
+++ app/src/main/java/id/rashio/android/ui/screen/articles/article_detail/ArticleDetailScreen.kt
@@ -3,6 +3,8 @@ package id.rashio.android.ui.screen.articles.article_detail
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -69,7 +71,11 @@ fun ArticleDetailScreen(
         },
         content = { padding ->
             Column(
-                modifier = Modifier.padding(padding)
+                modifier = Modifier
+                    .padding(padding)
+                    .verticalScroll(
+                        rememberScrollState()
+                    )
             ) {
                 BannerArticle(imageUrl = article.imageUrl, title = article.title)
                 ArticleInfo(
